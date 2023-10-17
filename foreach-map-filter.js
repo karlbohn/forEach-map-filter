2aa5a22f7b5569a2ae2bf3477306a9dbcfade577
Karl Bohn <karl@McBain>
2023-10-17 00:35:02 -0400

    doubleValues and onlyEvenValues working

--- foreach-map-filter.js
+++ foreach-map-filter.js
@@ -1,13 +1,7 @@
-/*
-Write a function called doubleValues which accepts an array and returns a new array with all the values in the array passed to the function doubled
-
-Examples:
-    doubleValues([1,2,3]) // [2,4,6]
-    doubleValues([5,1,2,3,10]) // [10,2,4,6,20]
-
-*/
 function doubleValues(arr){
-    
+    return arr.map(function(num) {
+        return num * 2;
+    });
 }
 
 /*
@@ -19,7 +13,9 @@ Examples:
 
 */
 function onlyEvenValues(arr){
-    
+    return arr.filter(function(num){
+        return num % 2 === 0;
+    })
 }
 
 /*
